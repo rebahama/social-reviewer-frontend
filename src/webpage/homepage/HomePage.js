2f0340c807c3c3064cf4bda08cc1d52b6376b9c2
rebahama <wiri@hotmail.se>
2022-09-14 01:53:02 +0000

    add slideshow on homepage

--- src/webpage/homepage/HomePage.js
+++ src/webpage/homepage/HomePage.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import smiley from '../../assets/smiley.jpg'
-import logo from '../../assets/Logo-social-reviewer.png'
 import { Row, Col, Container, Carousel } from 'react-bootstrap'
 import styles from '../../styles/HomePage.module.css'
+import SlideShow from './SlideShow'
 
 
 function HomePage() {
@@ -13,48 +12,12 @@ function HomePage() {
                 <h1 className={styles.HeroText}> Social Reviewer, the review site for all reviews!</h1>
             </Row>
             <h2> Check out our Most popular posts</h2>
-            <Row>
-                <Carousel fade>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={logo}
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={logo}
-                            alt="Second slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={logo}
-                            alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                            </p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </Row>
+            <hr/>
+            <SlideShow/>
         </Container>
+        
+
+        
     )
 }
 
