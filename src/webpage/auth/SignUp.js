3b4580f5c857f4678cce87bcbcd72985a1c29281
rebahama <wiri@hotmail.se>
2022-09-15 04:38:25 +0000

    add more style to signup page

--- src/webpage/auth/SignUp.js
+++ src/webpage/auth/SignUp.js
@@ -1,99 +1,96 @@
 import axios from 'axios';
-import React, {useState} from 'react'
-import {Alert, Button, Container, Form, Col, Row } from'react-bootstrap'
+import React, { useState } from 'react'
+import { Alert, Button, Container, Form, Col, Row } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import rocket from '../../assets/rocket.png'
 import styles from '../../styles/SignUp.module.css'
 
 
 const SignUp = () => {
-    const [signIn, setSignIn] = useState({
-        username:'',
-        password1:'',
-        password2:'',
+  const [signIn, setSignIn] = useState({
+    username: '',
+    password1: '',
+    password2: '',
 
 
-    });
-    const {username, password1, password2} = signIn
-    const history = useHistory()
-    const [error, setError] = useState({})
+  });
+  const { username, password1, password2 } = signIn
+  const history = useHistory()
+  const [error, setError] = useState({})
 
 
 
-const handleInput = (event) => {
+  const handleInput = (event) => {
     setSignIn({
-        ...signIn,
-        [event.target.name]: event.target.value,
+      ...signIn,
+      [event.target.name]: event.target.value,
     });
-};
+  };
 
-const submitForm = async (event) =>{
+  const submitForm = async (event) => {
 
     event.preventDefault();
     try {
-        await axios.post("/dj-rest-auth/registration/", signIn) 
-        history.push('/')
-    }catch(err){
-        setError(err.response?.data)
+      await axios.post("/dj-rest-auth/registration/", signIn)
+      history.push('/')
+    } catch (err) {
+      setError(err.response?.data)
     }
 
-}
+  }
 
 
-    return (
+  return (
 
 
-<Container>
-  <Row>
-        <Form onSubmit={submitForm}>
-      <h1> Are you ready for take of? Please create an account to get started</h1>
-      <img src={rocket} className={styles.RocketImage}></img>
-        <Form.Group className="mb-3" controlId="username">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Username" value={username} name="username" onChange={handleInput} />
-        </Form.Group>
+    <Container >
+      <div className={styles.AllContent}>
+        <Row md={12}  >
+          <Col md={6}>
+            <Form onSubmit={submitForm}>
+              <h1> Are you ready for take of? Please create an account to get started</h1>
+              <Form.Group className="mb-3" controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Username" value={username} name="username" onChange={handleInput} />
+              </Form.Group>
 
-        {error.username?.map((message, idx)=>
-        <Alert variant="warning" key={idx}> {message} </Alert>
-        
-        )}
-        <Form.Group className="mb-3" controlId="password1">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" value={password1} name="password1" onChange={handleInput} />
-        </Form.Group>
-        
-        {error.password1?.map((message, idx)=>
-        <Alert variant="warning" key={idx}> {message} </Alert>
-        
-        )}
-      
+              {error.username?.map((message, idx) =>
+                <Alert variant="warning" key={idx}> {message} </Alert>
 
+              )}
+              <Form.Group className="mb-3" controlId="password1">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={password1} name="password1" onChange={handleInput} />
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="password2">
-          <Form.Label>Confirm password</Form.Label>
-          <Form.Control type="password" placeholder="Confirm password" value={password2} name="password2" onChange={handleInput} />
-        </Form.Group>
-        {error.password2?.map((message, idx)=>
-        <Alert variant="warning" key={idx}> {message} </Alert>
-        
-        )}
+              {error.password1?.map((message, idx) =>
+                <Alert variant="warning" key={idx}> {message} </Alert>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+              )}
+              <Form.Group className="mb-3" controlId="password2">
+                <Form.Label>Confirm password</Form.Label>
+                <Form.Control type="password" placeholder="Confirm password" value={password2} name="password2" onChange={handleInput} />
+              </Form.Group>
+              {error.password2?.map((message, idx) =>
+                <Alert variant="warning" key={idx}> {message} </Alert>
 
-        {error.non_field_errors?.map((message, idx)=>
-        <Alert variant="warning" key={idx}> {message} </Alert>
-        
-        )}
-      </Form>
-      </Row>
-      </Container>
-
+              )}
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+              {error.non_field_errors?.map((message, idx) =>
+                <Alert variant="warning" key={idx}> {message} </Alert>
+              )}
+            </Form>
+          </Col>
+        </Row>
+        <img src={rocket} className={styles.RocketImage}></img>
+      </div>
+    </Container>
 
 
 
-    )
+  )
 }
 
 export default SignUp
