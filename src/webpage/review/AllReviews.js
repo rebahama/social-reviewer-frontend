672d3e088cb7bf9fb2d0a8dd6995b7eb5fdbcb91
rebahama <wiri@hotmail.se>
2022-09-23 02:42:05 +0000

    add show all reviews page

--- src/webpage/review/AllReviews.js
+++ src/webpage/review/AllReviews.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const AllReviews = () => {
+import { axiosReq } from '../../api/axios';
+import ReviewPage from './ReviewPage';
+
+const AllReviews = (props) => {
+
+  const {
+    
+    owner
+  
+
+
+
+ } = props
+  const [review, setReview] = useState({results: []});
+
+  useEffect(()=>{
+    const handleData = async () =>{
+        try{
+          const { data } = await axiosReq.get("/posts/");
+        setReview(data)
+        console.log(review)
+        }
+        catch(err){
+            console.log(err)
+
+        }
+
+    }
+    handleData()
+
+}, [])
+
+  
+  
   return (
-    <div>AllReviews
+    <div>
 
-
+{review.results.map((review) => (
+                  <ReviewPage key={review.id} {...review}/>
+                ))}
       
     </div>
   )
