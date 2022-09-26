1322845374fd9750c56153d73fdc8796c6872875
rebahama <wiri@hotmail.se>
2022-09-26 20:53:58 +0000

    add a search function

--- src/webpage/review/AllReviews.js
+++ src/webpage/review/AllReviews.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from 'react'
 import { axiosReq } from '../../api/axios';
 import ReviewPage from './ReviewPage';
 import SpinnerAsset from '../../components/SpinnerAsset'
+import { Form } from 'react-bootstrap';
 
 const AllReviews = (props) => {
 
   const {id} = props
   const [review, setReview] = useState({ results: [] });
   const [loaded, loadedcomplete] = useState(false)
+  const [query, setQuery] = useState("")
 
   useEffect(() => {
     const handleData = async () => {
       try {
-        const { data } = await axiosReq.get("/posts/");
+        const { data } = await axiosReq.get(`/posts/?search=${query}`);
         setReview(data)
         loadedcomplete(true)
         console.log(review)
@@ -26,13 +28,17 @@ const AllReviews = (props) => {
     loadedcomplete(false)
     handleData()
 
-  }, [id])
+  }, [id,query])
 
 
 
   return (
     <div>
+      <Form onSubmit={(event)=> event.preventDefault()}>
+    <Form.Control type="text" value={query} onChange={(event)=> setQuery(event.target.value)}/> 
 
+
+      </Form>
       {loaded ?
         (<>{review.results.map((review) => (<ReviewPage key={review.id} {...review} />))} </>) : (<SpinnerAsset />) }
     </div>
