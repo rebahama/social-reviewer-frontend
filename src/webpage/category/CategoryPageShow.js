7760050c9e030eed13d51af7466cccf9ab4b4b09
rebahama <wiri@hotmail.se>
2022-09-28 00:34:37 +0000

    fetch api based on the id for the categorypage

--- src/webpage/category/CategoryPageShow.js
+++ src/webpage/category/CategoryPageShow.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import { axiosReq } from '../../api/axios'
 
 function CategoryPageShow() {
-  return (
-    <div>CategoryPageShow</div>
+  const [category, setCategory] = useState({results: []})
+  const {id} = useParams()
+  useEffect(()=>{
+
+const handleData = async()=>{
+
+    try {
+        const {data} = await axiosReq.get(`/posts/?owner__profile=&category=${id}`)
+            setCategory(data)
+            console.log(data)
+
+    }
+    catch(err){
+        console.log(err)
+    }
+
+
+
+}
+handleData()
+
+
+  }, [])
+
+  
+    return (
+    <div>CategoryPageShow
+{category.results.map(post=>
+       
+        <p key={post.id}> {post.title} {post.id}</p>
+    
+        )}
+
+
+    </div>
   )
 }
 
