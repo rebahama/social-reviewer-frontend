f00849f1b1bbcfbf2b9f86a285787ef41620d9cf
rebahama <wiri@hotmail.se>
2022-10-03 19:29:15 +0000

    add functionality for sorting the reviews after price, from highest to lowest

--- src/webpage/review/AllReviews.js
+++ src/webpage/review/AllReviews.js
@@ -3,7 +3,8 @@ import { axiosReq } from '../../api/axios';
 import ReviewPage from './ReviewPage';
 import styles from '../../styles/ReviewPage.module.css'
 import SpinnerAsset from '../../components/SpinnerAsset'
-import { Col, Container, Form, Row } from 'react-bootstrap';
+import { Col, Container, Form, Row, Button } from 'react-bootstrap';
+import SortByPrice from './SortByPrice';
 
 const AllReviews = (props) => {
 
@@ -11,7 +12,8 @@ const AllReviews = (props) => {
   const [review, setReview] = useState({ results: [] });
   const [loaded, loadedcomplete] = useState(false)
   const [query, setQuery] = useState("")
-
+  const [clicked, setClick] = useState(false)
+  
   useEffect(() => {
     const handleData = async () => {
       try {
@@ -41,7 +43,10 @@ const AllReviews = (props) => {
 
   }, [id, query])
 
+const handleSort =()=>{
+  setClick(true)
 
+}
 
   return (
     <div>
@@ -51,10 +56,17 @@ const AllReviews = (props) => {
             <Form onSubmit={(event) => event.preventDefault()}>
               <Form.Control type="text" className={styles.SearchBar} placeholder="Search a review" value={query} onChange={(event) => setQuery(event.target.value)} />
             </Form>
+            
           </Col>
         </Row>
       </Container>
-      {loaded ? (<> {review.results.map((review) => (<ReviewPage key={review.id} {...review} />))} </>) : (<SpinnerAsset />)}
+      
+     
+     {clicked ? (<SortByPrice/>):<> <Button onClick={handleSort}> Sort by price </Button> {review.results.map((review) => (<ReviewPage key={review.id} {...review} />))}  <SpinnerAsset/> </> } 
+    
+     
+     
+     
     </div>
   )
 }
---
+++ src/webpage/review/SortByPrice.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { axiosReq } from '../../api/axios'
+import ReviewPage from './ReviewPage';
+
+function SortByPrice() {
+    const [sortReview, setSortReview] = useState({ sortedReviews: { results: [] } })
+    const {sortedReviews} =sortReview
+
+    useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const { data } = await axiosReq.get("posts/?ordering=-price");
+            setSortReview((prevState) => ({
+              ...prevState,
+              sortedReviews: data,
+            }));
+          } catch (err) {
+            console.log(err);
+          }
+        };
+    
+        handleMount();
+      }, []);
+  
+  
+    return (
+    <div>
+        <p> Reviews are sorted by price from highest to lowest </p>
+        {sortedReviews.results.map((sort)=>
+        {return <ReviewPage key={sort.id} {...sort}/>})}
+
+
+
+    </div>
+  )
+}
+
+export default SortByPrice
