e304a42dd3041ae06e2f60a56087f91d65f8abb3
rebahama <wiri@hotmail.se>
2022-10-05 01:40:14 +0000

    add target_blank to footer icon links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,22 @@
 import React from 'react'
-import {Container} from 'react-bootstrap'
 import styles from '../styles/Footer.module.css'
 
 function Footer() {
-  return (
-  
-        <div className={styles.Footer}>
-      <ul>
-        <li className="list-inline-item"><a href="https://sv-se.facebook.com/" target="_blank"><i
-              className="fa-brands fa-facebook"> </i></a></li>
-        <li className="list-inline-item"><a href="https://www.instagram.com/" target="_blank"><i
-              className="fa-brands fa-instagram"> </i></a></li>
-        <li className="list-inline-item"><a href="https://twitter.com/?lang=sv" target="_blank"><i
-              className="fa-brands fa-twitter"> </i></a></li>
-        <li className="list-inline-item"><a href="https://www.linkedin.com/" target="_blank"><i
-              className="fa-brands fa-linkedin"> </i></a></li>
-        </ul>
-    </div>
-
-
-        
-   
-  )
+      /* Display a footer*/
+      return (
+            <div className={styles.Footer}>
+                  <ul>
+                        <li className="list-inline-item"><a href="https://sv-se.facebook.com/" target="_blank"rel=" noopener noreferrer"><i
+                              className="fa-brands fa-facebook"> </i></a></li>
+                        <li className="list-inline-item"><a href="https://www.instagram.com/" target="_blank" rel=" noopener noreferrer"><i
+                              className="fa-brands fa-instagram"> </i></a></li>
+                        <li className="list-inline-item"><a href="https://twitter.com/?lang=sv" target="_blank" rel="noopener noreferrer"><i
+                              className="fa-brands fa-twitter"> </i></a></li>
+                        <li className="list-inline-item"><a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i
+                              className="fa-brands fa-linkedin"> </i></a></li>
+                  </ul>
+            </div>
+      )
 }
 
 export default Footer
