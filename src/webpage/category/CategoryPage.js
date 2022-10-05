1c57f632b223fd2fc42584da0d5be2a70357c564
rebahama <wiri@hotmail.se>
2022-10-05 02:19:09 +0000

    add semicolons and comments to categorypage.js

--- src/webpage/category/CategoryPage.js
+++ src/webpage/category/CategoryPage.js
@@ -1,20 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import styles from '../../styles/CategoryPage.module.css'
-import reviewGif from '../../assets/review-rating.gif'
-import commentGif  from '../../assets/comment.gif'
-import signupGif from '../../assets/sign-up.gif'
-import {Carousel, Container, } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import styles from '../../styles/CategoryPage.module.css';
+import reviewGif from '../../assets/review-rating.gif';
+import commentGif from '../../assets/comment.gif';
+import signupGif from '../../assets/sign-up.gif';
+import { Carousel, Container, } from 'react-bootstrap';
 import logo from '../../assets/Logo-social-reviewer.png'
 import { Link } from 'react-router-dom';
-import { axiosReq } from '../../api/axios'
+import { axiosReq } from '../../api/axios';
 import SpinnerAsset from '../../components/SpinnerAsset';
 import MostLikedReview from '../review/MostLikedReview';
 import MostCommentedReview from '../review/MostCommentedReview';
 
 const CategoryPage = () => {
+/** Function for category, when the category is not loaded display a spinner until the get request
+ * is completed.
+ */
+  const [loaded, loadedcomplete] = useState(false);
+  const [category, setCategory] = useState({ results: [] });
 
-  const [loaded, loadedcomplete] = useState(false)
-  const [category, setCategory] = useState({ results: [] })
   useEffect(() => {
 
     const handleData = async () => {
@@ -43,15 +46,14 @@ const CategoryPage = () => {
       clearTimeout(time)
     }
 
-  }, [])
-
+  }, []);
 
   return (
+
     <div>
       <Container>
         <Carousel>
           <Carousel.Item>
-
             <img
               className="d-block w-100"
               src={logo}
@@ -75,7 +77,6 @@ const CategoryPage = () => {
               <MostLikedReview />
             </Carousel.Caption>
           </Carousel.Item>
-
           <Carousel.Item className={styles.ContainerCarouselThird}>
             <h3 className={styles.PopularText}> Most Commented Reviews </h3>
             <img
@@ -88,25 +89,20 @@ const CategoryPage = () => {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item className={styles.ContainerCarouselFourth}>
-            
-          <h3 className={styles.PopularText}> Checkout the reviews</h3>
-          <img
+            <h3 className={styles.PopularText}> Checkout the reviews</h3>
+            <img
               className="d-block w-50"
               src={signupGif}
               alt="First slide"
             />
-<Carousel.Caption>
-<Link to="/reviews" className={styles.BtnCarouselFourth}> All reviews</Link>
-
-</Carousel.Caption>
-
-
+            <Carousel.Caption>
+              <Link to="/reviews" className={styles.BtnCarouselFourth}> All reviews</Link>
+            </Carousel.Caption>
           </Carousel.Item>
-
-
         </Carousel>
       </Container>
     </div>
+    
   )
 }
 
