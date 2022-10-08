36e7ff26c085ddb90577fb2dd26d3eb47ecc335e
rebahama <wiri@hotmail.se>
2022-10-08 14:36:25 +0000

    show welcome message only if user is logged in

--- src/webpage/homepage/HomePage.js
+++ src/webpage/homepage/HomePage.js
@@ -39,8 +39,7 @@ function HomePage() {
                 <h1 className={styles.HeroText}> Social Reviewer, the review site for all reviews!</h1>
             </Row>
             <hr />
-
-            {alertMessage && <p> welcome {currentUser?.username}</p>}
+            {currentUser? alertMessage && <p> Welcome {currentUser?.username}</p>:""}
             <h2 className={styles.PresentationText}> Welcome to social reviewer, Are you tired to compare and search after diffrent reviews on many diffrent pages. Not anymore now you can find reviews about everything from cars to electronics, on the same page. Create an account to get started or checkout our reviews</h2>
 
             <CategoryPage />
