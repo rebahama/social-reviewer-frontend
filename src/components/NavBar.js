615cd4166f752d7f09bc3197a16a3a56536116cb
rebahama <wiri@hotmail.se>
2022-10-08 05:48:37 +0000

    show message for 5 seconds that user have been logged out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styles from '../styles/NavBar.module.css';
-import { Navbar, Nav, Button, Modal, } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../context/CurrentUserContext';
 import axios from 'axios';
@@ -11,15 +11,21 @@ import { removeTokenTimestamp } from '../utilis/utilis';
 const NavBar = () => {
     /** This navbar is a component that will be visable thorugh out the whole application.
      * Depending on the state of the user diffrent links will be visable to the site user. */
- 
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const setCurrentUser = useSetCurrentUser()
 
+
+
+    const setCurrentUser = useSetCurrentUser()
+    const [alertMessage, setAlertMessage] = useState(false)
+    
     const handleLogOut = async () => {
         try {
             await axios.post("dj-rest-auth/logout/");
+            setAlertMessage(true)
+
+            setTimeout(() => {
+                setAlertMessage(false);
+            }, 5000);
+
             setCurrentUser(null);
             removeTokenTimestamp();
         }
@@ -33,31 +39,21 @@ const NavBar = () => {
     const currentUser = useCurrentUser();
     const UserName = (<NavLink to={`/profiles/${currentUser?.profile_id}`} className={styles.NavLink}>  <i className="fa-solid fa-user"> </i> Username: {currentUser?.username} </NavLink>);
     const emptyUsername = "";
+
+
     const loggedIn = <>
+
         <NavLink to="/myreviews" className={styles.NavLink}> <i className="fa-solid fa-bars"></i> My reviews</NavLink>
         <NavLink className={styles.NavLink} to="/createreview"><i className="fa-sharp fa-solid fa-plus"></i> Create review </NavLink>
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Logging out </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Are you sure you want to log out? </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    No
-                </Button>
-                <NavLink onClick={handleLogOut} to={`/`}>
-                    <Button variant="danger"> Yes </Button> </NavLink>
-            </Modal.Footer>
-        </Modal>
-        <NavLink to="/" className={styles.NavLink} onClick={handleShow}> <i className="fa-solid fa-arrow-right-from-bracket"> </i> Log out</NavLink>
+        <NavLink to="/" className={styles.NavLink} onClick={handleLogOut}> <i className="fa-solid fa-arrow-right-from-bracket"> </i> Log out</NavLink>
     </>;
 
 
-    const loggedOut =(<>
+
+    const loggedOut = (<>
         <NavLink to="/signin" className={styles.NavLink}><i className="fa-solid fa-arrow-right-to-bracket"></i> Sign in</NavLink>
         <NavLink to="/signup" className={styles.NavLink}><i className="fa-solid fa-users"> </i> Sign Up</NavLink>
     </>);
-
 
     return (
         <Navbar bg="light" variant="light" className={styles.NavBar} expand="lg">
@@ -73,6 +69,7 @@ const NavBar = () => {
                     {currentUser ? loggedIn : loggedOut}
                     {currentUser ? UserName : emptyUsername}
                 </Nav>
+                {alertMessage && <p> You have been logged out</p>}
             </Navbar.Collapse>
         </Navbar>
     )
